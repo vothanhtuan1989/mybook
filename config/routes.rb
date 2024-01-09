3b4c4ec67aa9865d433dd9c57fce93d8f4069b8a
Tuan Vo <vothanhtuan1989@gmail.com>
2024-01-09 10:17:49 +0700

    added devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get 'home/index'
+
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks'
+  }
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(
+  email: "admin@book.slotgroup.com",
+  password: "qWZ*6uAg25H8!4",
+  root_user: true
+)
